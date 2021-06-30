de92bfc55af478360699f40a80a9fa323e676fbb
Apattt <86716020+Apattt@users.noreply.github.com>
2021-06-30 17:10:07 +0700

    Update config.js

--- config.js
+++ config.js
@@ -4,7 +4,7 @@ const CONFIG = {
     btnIntro: 'hihii',
     title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 😙',
     desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
-    btnYes: 'Vẫn cứ là thích anh <33',
+    btnYes: 'thích anh <33',
     btnNo: 'Không thíchhh :3',
     question:'Trên thế giới hơn 7 tỉ người mà sao em lại yêu anh <3',
     btnReply: 'Gửi cho anh <3',
@@ -12,5 +12,5 @@ const CONFIG = {
     mess: 'Anh biết mà 🥰. Yêu em nhiều nhiều 😘😘',
     messDesc: 'Tối nay 10h nhà anh nhé công chúa.',
     btnAccept: 'Okiiiii lunnnnnn <3',
-    messLink: 'https://l.facebook.com/l.php?u=https%3A%2F%2Fm.me%2Fphat.apat.12%3Ffbclid%3DIwAR1fFLIA-c-75hIz9rSCHqyZ7Zbn0ajsk3ax1UOaP2tHp1rZrjXaMYOsYnQ&h=AT1FRT8B5gp7AtxSoMYRtzsfetMkStZnItpqy2ephClkhQIZZqdUtSnLMQ6MQHxTTei6lmjLqYXLk2qWNYlT3M53p4EEisgBKtWjgyW83YAIkcifs7Qlonfh2A_Ne1LcOd4iStX0-YUWdmFC6yGffg' //link mess của các bạn. VD: https://m.me/nam.nodemy
+    messLink: 'https://www.facebook.com/phat.apat.12/' //link mess của các bạn. VD: https://m.me/nam.nodemy
 }
